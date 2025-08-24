c3cf5c0efa1b3c45bfde146c03bf90fd4147e73d
Pablo Cuellar <90666361+PablinCu02@users.noreply.github.com>
2025-08-23 23:58:27 -0600

    PruebaLetraP_Ejercicio2

--- segundo_main.cpp
+++ segundo_main.cpp
@@ -126,6 +126,43 @@ void CompileShaders() {
 	}
 }
 
+void crearIniciales() {
+	GLfloat vertices[] = {
+		//Letra P
+		// 2 triangulos para la barra vertical izq
+		-0.75f,  0.60f, 0.0f,   -0.70f,  0.60f, 0.0f,   -0.70f, -0.60f, 0.0f,
+		-0.75f,  0.60f, 0.0f,   -0.70f, -0.60f, 0.0f,   -0.75f, -0.60f, 0.0f,
+
+		// 2 triangulos para la barra superior 
+		-0.75f,  0.60f, 0.0f,   -0.45f,  0.60f, 0.0f,   -0.45f,  0.50f, 0.0f,
+		-0.75f,  0.60f, 0.0f,   -0.45f,  0.50f, 0.0f,   -0.75f,  0.50f, 0.0f,
+
+		// 2 triangulos para la barra lateral derecha superior
+		-0.50f,  0.60f, 0.0f,   -0.45f,  0.60f, 0.0f,   -0.45f,  0.20f, 0.0f,
+		-0.50f,  0.60f, 0.0f,   -0.45f,  0.20f, 0.0f,   -0.50f,  0.20f, 0.0f,
+
+		// 2 triangulos para la barra horizontal del medio
+		-0.75f,  0.25f, 0.0f,   -0.50f,  0.25f, 0.0f,   -0.50f,  0.15f, 0.0f,
+		-0.75f,  0.25f, 0.0f,   -0.50f,  0.15f, 0.0f,   -0.75f,  0.15f, 0.0f,
+
+		// 2 triangulos para la barra lateral derecha inferior
+		-0.50f,  0.20f, 0.0f,   -0.45f,  0.20f, 0.0f,   -0.45f,  0.15f, 0.0f,
+		-0.50f,  0.20f, 0.0f,   -0.45f,  0.15f, 0.0f,   -0.50f,  0.15f, 0.0f,
+	};
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// ¡Usa sizeof(GLfloat)!
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
 
 int main()
 {
@@ -182,8 +219,9 @@ int main()
 	glViewport(0, 0, BufferWidth, BufferHeight);
 
 	//Llamada a las funciones creadas antes del main
-	CrearFiguras();// Comentar (EJERCICIO 2) / Descomentar (EJERCICIO 1)
+	//CrearFiguras();// Comentar (EJERCICIO 2) / Descomentar (EJERCICIO 1)
 	CompileShaders();
+	crearIniciales();
 
 
 	//Loop mientras no se cierra la ventana
@@ -209,7 +247,7 @@ int main()
 		glUseProgram(shader);
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 12); // 12 vértices (el cuadrado y el rombo)
+		glDrawArrays(GL_TRIANGLES, 0, 30); // 12 vértices (el cuadrado y el rombo)
 		glBindVertexArray(0);
 
 		glUseProgram(0);
